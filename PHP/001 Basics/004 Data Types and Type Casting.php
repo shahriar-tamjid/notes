aa9b0c5181a22f1db37d6e35464d4e20bad62faf
Shahriar Tamjid <65736523+shahriar-tamjid@users.noreply.github.com>
2023-01-20 23:48:52 +0600

    Data Types and Type Casting

---
+++ PHP/001 Basics/004 Data Types and Type Casting.php
@@ -0,0 +1,17 @@
+<?php
+
+// 4 Scalar Types
+
+// bool - true / false
+$completed = true;
+// int - 1,2,3,0,-5 (no decimal)
+$score = 75;
+// float - 1.5,0.1,0.005,-15.8
+$price = 0.99;
+// string - "Shahriar", "Hello World!"
+$greeting = "Hello Shahriar";
+
+echo $completed; // Output: 1
+echo $score; // Output: 75
+echo $price; // Output: 0.99
+echo $greeting; // Output: Hello Shahriar
